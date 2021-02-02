935342c1198a575028d34ee511351f6d4ad4039e
Falcon <gerardo.lopez@edify.cr>
2021-02-02 14:58:55 -0600

    Add cache images

--- src/sw-custom.js
+++ src/sw-custom.js
@@ -11,6 +11,32 @@ if (workbox) {
     // Ignore all URL parameters.
     ignoreURLParametersMatching: [/.*/]
   });
+
+  workbox.routing.registerRoute(
+    /https:\/\/rickandmortyapi.com\/api\/character(?!\/avatar)/,
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'rickandmortyapi-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 20
+        })
+      ]
+    })
+  );
+
+  workbox.routing.registerRoute(
+    /https:\/\/rickandmortyapi\.com\/api\/character\/avatar\/(.+)\.(?:jpeg|jpg)/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'avatar-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 20,
+          maxAgeSeconds: 7 * 24 * 60 * 60 // 1 week
+        })
+      ]
+    })
+  );
+
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
